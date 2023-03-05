7f75c90ebaad973eccfd6aff1019bce0e8df1422
MasonShi <shiyujiesx@163.com>
2023-03-05 17:30:42 +0000

    accidently ignore folders, intention is only files

---
+++ GitActivity/Merge_Rebase/common.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+   printf("Test.");
+/*
+<<<<<<< HEAD
+   printf("Edit this file by mason on mac");
+=======
+*/
+   printf("Edit this file by mason on linux");
+// >>>>>>> 451e7b8 (change by mason on linux vagrant)
+
+   return 0;
+}
