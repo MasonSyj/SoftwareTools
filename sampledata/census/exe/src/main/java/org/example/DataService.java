02a8c036e7b1443e8847b894a7b24ede97880e0c
MasonSyj <shiyujiesx@163.com>
2023-03-08 09:51:38 +0000

    jdbc sqlite java suddently work but doesn't know why

--- sampledata/census/exe/src/main/java/org/example/DataService.java
+++ sampledata/census/exe/src/main/java/org/example/DataService.java
@@ -25,72 +25,26 @@ public class DataService implements AutoCloseable{
         }
 
     }
-/*
-
-    public List<Party> getParties(){
-        List<Party> parties = new ArrayList<>();
-
-        try (PreparedStatement s = c.prepareStatement("SELECT id, name FROM Party")) {
-            ResultSet r = s.executeQuery();
-            while (r.next()) {
-                int id = r.getInt("id");
-                String name = r.getString("name");
-                parties.add(new Party(id, name));
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return parties;
-    }
-
-    public Party getParty(int id){
-        Party res;
-        try (PreparedStatement s = c.prepareStatement("SELECT id, name FROM Party WHERE id = ?")) {
-            s.setInt(1, id);
-            ResultSet r = s.executeQuery();
-            r.next();
-            String name = r.getString("name");
-            res = new Party(id, name);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return res;
-    }
-
-    public Candidate getCandidate(int id){
-        Candidate res;
-        try (PreparedStatement s = c.prepareStatement("SELECT Candidate.id AS id, Candidate.name AS name, Party.name AS party, Ward.name AS ward, votes, Candidate.party AS pid, Candidate.ward AS wid FROM Party, Candidate, Ward WHERE Candidate.party = Party.id AND Candidate.ward = Ward.id AND Candidate.id = ?")) {
-            s.setInt(1, id);
-            ResultSet r = s.executeQuery();
-            r.next();
-            String name = r.getString("name");
-            String party = r.getString("party");
-            String ward = r.getString("ward");
-            int votes = r.getInt("votes");
-            int pid = r.getInt("pid");
-            int wid = r.getInt("wid");
-            res = new Candidate(id, name, new Party(pid, party), new Ward(wid, ward), votes);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return res;
-    }
-*/
 
     public static void main(String[] args) throws SQLException {
 
-        DataService test = new DataService("jdbc:sqlite:/vagrant/sampledata/census/census.db");
+        DataService test = new DataService("jdbc:sqlite:/home/tj22981/vagrant/sampledata/census/census.db");
 
-//        Candidate candidate1 = test.getCandidate(2456);
-        try(PreparedStatement s = test.c.prepareStatement("SELECT * FROM Region;")){
+        try(PreparedStatement s = test.c.prepareStatement("SELECT * FROM Region")){
             ResultSet r = s.executeQuery();
-            r.next();
-            String code = r.getString("code");
-            String parent = r.getString("parent");
-            String name = r.getString("name");
+
+            while (r.next()) {
+                String code = r.getString("code");
+                String parent = r.getString("parent");
+                String name = r.getString("name");
+                System.out.println(code + ", " + parent + ", " + name);
+            }
+
         } catch (SQLException e){
             throw new RuntimeException(e);
         }
+
+        test.close();
 
     }
 }
