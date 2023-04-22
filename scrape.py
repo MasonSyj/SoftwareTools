d4fd7fcd8e47a28a90304d5890ae66d7e476aa92
MasonSyj <shiyujiesx@163.com>
2023-04-22 20:30:03 +0100

    mdict.get('flowername')

--- scrape.py
+++ scrape.py
@@ -2,17 +2,20 @@ from bs4 import BeautifulSoup
 import os
 
 
-cattaxDict = {}
+mdict = {}
 
 for file in os.listdir('cattax'):
   if file[-4:] == 'html':
     soup = BeautifulSoup(open('cattax/'+file,'r'), features='html.parser')
     container = soup.find('div', class_='container')
     if container:
+      title = soup.title.text
+      print(title)
       head = soup.title.text + " : " + soup.h1.text
       info = soup.find('p', class_='info').text
-      cattaxDict[head] = info
+      mdict[title] = info
       # print(soup.title.text + " : " + soup.h1.text)
       # print(soup.find('p', class_='info').text)
+      # in interact mode it will be, mdict.get('flowerName')
 
    
